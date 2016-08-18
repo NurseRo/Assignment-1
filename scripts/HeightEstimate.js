7bcb41dde4c8482ea278f1521372d6d87cf8d60a
NurseRo <rvar14@student.monash.edu>
2016-08-18 14:10:22 +1000

    HouseKeeping 3

--- scripts/HeightEstimate.js
+++ scripts/HeightEstimate.js
@@ -16,7 +16,7 @@
 // cameraVideoPageInitialised() function when ready.
 var cameraVideoPage = new CameraVideoPageController(
         cameraVideoPageInitialised);
-
+var heightVal;
 // You may need to create variables to store state.
     
 // This function will be called when the camera video page
@@ -25,7 +25,6 @@ function cameraVideoPageInitialised()
 {
     // Step 1: Check for and intialise deviceMotion
     //Initiates device motion
-    window.addEventListener("devicemotion", motionhandler, true);
 }
 function motionhandler(){
 }
@@ -35,16 +34,19 @@ function setCameraHeightValue()
 {
    
 	// Step 3: Set camera height
-	heightval=window.prompt("Please enter the height of the camera from the ground (meters):")
+	heightVal = window.prompt("Please enter the height of the camera from the ground (meters):");
 	// check if input is a number and is positive
-	if (heightval.isnumber=true & heightval>=0)
+	if (heightVal.isnumber = true && heightVal > 0)
 	{
         // display on screen using the displayMessage method
-		cameraVideoPage.displayMessage("Height of camera: " + heightval + "m",3000)
-		cameraVideoPage.setHeadsUpDisplayHTML("Height of camera: " + heightval + "m")
+		cameraVideoPage.displayMessage("Height of camera: " + heightVal + "m",3000)
+		cameraVideoPage.setHeadsUpDisplayHTML("Height of camera: " + heightVal + "m")
 	}
 	
-	else {cameraVideoPage.displayMessage("Input is invalid, please try again",3000)}
+	else 
+    {
+        cameraVideoPage.displayMessage("Input is invalid, please try again.",3000)
+    }
 	
 }
     
